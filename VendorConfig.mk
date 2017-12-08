3568b30721d72f056f24e46678fdec005126a2fd
Mister Oyster <oysterized@gmail.com>
2017-12-08 16:26:24 +0100

    seccomp: add mediaextractor.policy

--- VendorConfig.mk
+++ VendorConfig.mk
@@ -4,4 +4,5 @@ TARGET_SPECIFIC_HEADER_PATH := $(VND_MTK_PATH)/include
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-	$(VND_MTK_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+	$(VND_MTK_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+	$(VND_MTK_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
