a8846124f706aea5185abf935e787aa20db13c9d
Mister Oyster <oysterized@gmail.com>
2017-12-09 21:13:03 +0100

    configs: add telephony conf (goes to system/)

--- board/conn.mk
+++ board/conn.mk
@@ -12,6 +12,11 @@ PRODUCT_COPY_FILES += \
     $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf:vendor/etc/bluetooth/bt_stack.conf \
     $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.sqc:vendor/etc/bluetooth/bt_stack.conf.sqc
 
+# Telephony
+PRODUCT_COPY_FILES += \
+    $(VND_MTK_PATH)/configs/telephony/ecc_list.xml:system/etc/ecc_list.xml \
+    $(VND_MTK_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(VND_MTK_PATH)/configs/wifi/p2p_supplicant_overlay.conf:vendor/etc/wifi/p2p_supplicant_overlay.conf \
