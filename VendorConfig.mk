a6635db22bc3a2255a9c6401c829c97e2c9e183c
Mister Oyster <oysterized@gmail.com>
2017-12-09 16:18:50 +0100

    config: don't dex pre-opt

--- VendorConfig.mk
+++ VendorConfig.mk
@@ -2,6 +2,9 @@ VND_MTK_PATH := vendor/mediatek
 
 TARGET_SPECIFIC_HEADER_PATH := $(VND_MTK_PATH)/include
 
+# Disable dex pre-opt
+WITH_DEXPREOPT := false
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
 	$(VND_MTK_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
