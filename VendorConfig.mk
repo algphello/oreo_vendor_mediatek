65d2bf105561c5555f722288da4edb0399b00b12
Mister Oyster <oysterized@gmail.com>
2017-12-09 16:17:51 +0100

    config: move the gc tweak tag here

--- VendorConfig.mk
+++ VendorConfig.mk
@@ -12,3 +12,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+
+# GC tweak
+PRODUCT_TAGS += dalvik.gc.type-precise
