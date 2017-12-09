ea9b556e8366f3d093ef4f35f18512e36e266c38
Mister Oyster <oysterized@gmail.com>
2017-12-09 22:47:56 +0100

    configs: add audio_effects.conf (already edited to look for vendor/)

--- board/av.mk
+++ board/av.mk
@@ -3,3 +3,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+
+# Audio
+PRODUCT_COPY_FILES += $(VND_MTK_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
