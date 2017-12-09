f7a850283b0bb9cbbd21d3d23b29cf67c8226b35
Mister Oyster <oysterized@gmail.com>
2017-12-09 16:16:52 +0100

    config: move google codecs configs to common tree

--- VendorConfig.mk
+++ VendorConfig.mk
@@ -6,3 +6,9 @@ TARGET_SPECIFIC_HEADER_PATH := $(VND_MTK_PATH)/include
 PRODUCT_COPY_FILES += \
 	$(VND_MTK_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
 	$(VND_MTK_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
+# Google codecs
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
