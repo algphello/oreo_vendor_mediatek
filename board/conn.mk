f9ca9c7e736b7e404165e37f0797be8f298b8712
Mister Oyster <oysterized@gmail.com>
2017-12-09 21:08:31 +0100

    configs: fix path to vendor/ and add p2p/wpa supplicant conf

--- board/conn.mk
+++ board/conn.mk
@@ -1,13 +1,20 @@
 # GPS
 PRODUCT_COPY_FILES += \
-    $(VND_MTK_PATH)/configs/gps/slp_conf:system/etc/slp_conf \
-    $(VND_MTK_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
-    $(VND_MTK_PATH)/configs/gps/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
+    $(VND_MTK_PATH)/configs/gps/slp_conf:vendor/etc/slp_conf \
+    $(VND_MTK_PATH)/configs/gps/gps.conf:vendor/etc/gps.conf \
+    $(VND_MTK_PATH)/configs/gps/agps_profiles_conf2.xml:vendor/etc/agps_profiles_conf2.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
-    $(VND_MTK_PATH)/configs/bluetooth/auto_pair_devlist.conf:system/etc/bluetooth/auto_pair_devlist.conf \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.debug:system/etc/bluetooth/bt_stack.conf.debug \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/etc/bluetooth/bt_stack.conf.sqc
+    $(VND_MTK_PATH)/configs/bluetooth/auto_pair_devlist.conf:vendor/etc/bluetooth/auto_pair_devlist.conf \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.debug:vendor/etc/bluetooth/bt_stack.conf.debug \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_did.conf:vendor/etc/bluetooth/bt_did.conf \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf:vendor/etc/bluetooth/bt_stack.conf \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.sqc:vendor/etc/bluetooth/bt_stack.conf.sqc
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(VND_MTK_PATH)/configs/wifi/p2p_supplicant_overlay.conf:vendor/etc/wifi/p2p_supplicant_overlay.conf \
+    $(VND_MTK_PATH)/configs/wifi/wpa_supplicant.conf:vendor/etc/wifi/wpa_supplicant.conf \
+    $(VND_MTK_PATH)/configs/wifi/wpa_supplicant_overlay.conf:vendor/etc/wifi/wpa_supplicant_overlay.conf
+
