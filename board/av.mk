2f960686a64a7c8fc2219ef19531ee4d7a293f4c
Mister Oyster <oysterized@gmail.com>
2017-12-17 16:05:21 +0100

    audio: convert audio_effects.conf to .xml

--- board/av.mk
+++ board/av.mk
@@ -5,4 +5,4 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:vendor/etc/media_codecs_google_video_le.xml
 
 # Audio
-PRODUCT_COPY_FILES += $(VND_MTK_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
+PRODUCT_COPY_FILES += $(VND_MTK_PATH)/configs/audio/audio_effects.xml:system/etc/audio_effects.xml
