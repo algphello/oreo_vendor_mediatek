887327a79385584b39f978285184edd2f3b6687a
Alex Kalles <alexakis1997@gmail.com>
2017-12-20 15:32:07 +0200

    Uninstall: Proper clean of patches
    Doesn't need to reset the commit, just undo the changes with "checkout -- ."

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -10,10 +10,9 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo -e "\n Cleaning $dir patches...\n"
-	git reset --hard && git clean -df
+	git checkout -- . && git clean -df
 done
 
 # -----------------------------------
 echo -e "Done !\n"
 cd $rootdirectory
-
