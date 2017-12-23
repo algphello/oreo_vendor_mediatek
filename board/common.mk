3219a844496b787808a184cfa478e516b23b4ae0
danielhk <daniel.p6800@gmail.com>
2017-12-23 12:55:32 +0800

    Use software gatekeeper

--- board/common.mk
+++ board/common.mk
@@ -4,6 +4,9 @@ WITH_DEXPREOPT := false
 # GC tweak
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+# SW Gatekeeper
+BOARD_USE_SOFT_GATEKEEPER := true
+
 # Fun with flags
 BOARD_USES_MTK_HARDWARE := true
 
