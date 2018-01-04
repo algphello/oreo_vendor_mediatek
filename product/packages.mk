67872ce948500dea27472d242e5802314e2c75a0
Mister Oyster <oysterized@gmail.com>
2018-01-04 13:50:13 +0100

    prebuilts: add libstlport from N

--- product/packages.mk
+++ product/packages.mk
@@ -4,6 +4,9 @@ PRODUCT_PACKAGES += MeizuDoze
 # Ion
 PRODUCT_PACKAGES += libion
 
+PRODUCT_COPY_FILES += \
+$(VND_MTK_PATH)/prebuilts/libstlport/lib/libstlport.so:system/lib/libstlport.so \
+$(VND_MTK_PATH)/prebuilts/libstlport/lib64/libstlport.so:system/lib64/libstlport.so \
 # Gps
 PRODUCT_PACKAGES += \
     libcurl \
