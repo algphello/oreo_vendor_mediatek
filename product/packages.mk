173b9515f21ffa4bf244d40a8dd89ad201ffd764
Mister Oyster <oysterized@gmail.com>
2018-01-04 13:42:29 +0100

    packages: add wifi utilitary tools

--- product/packages.mk
+++ product/packages.mk
@@ -11,3 +11,11 @@ PRODUCT_PACKAGES += \
 
 # Wifi mtk wpa_supp lib
 PRODUCT_PACKAGES += lib_driver_cmd_mt66xx
+
+# Wifi tools
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wifilogd \
+    wificond
